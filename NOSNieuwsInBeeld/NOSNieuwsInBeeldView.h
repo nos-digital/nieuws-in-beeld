8ffec4de7f189d237bd31aa0675dc4681fd9c96e
Alvin Nutbeij <alvin@app-department.com>
2020-06-29 23:01:30 +0200

    Initial Commit

---
+++ NOSNieuwsInBeeld/NOSNieuwsInBeeldView.h
@@ -0,0 +1,13 @@
+//
+//  NOSNieuwsInBeeldView.h
+//  NOSNieuwsInBeeld
+//
+//  Created by Alvin Nutbeij on 29/06/2020.
+//  Copyright © 2020 App Department. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface NOSNieuwsInBeeldView : ScreenSaverView
+
+@end
---
+++ NOSNieuwsInBeeld/NOSNieuwsInBeeldView.m
@@ -0,0 +1,52 @@
+//
+//  NOSNieuwsInBeeldView.m
+//  NOSNieuwsInBeeld
+//
+//  Created by Alvin Nutbeij on 29/06/2020.
+//  Copyright © 2020 App Department. All rights reserved.
+//
+
+#import "NOSNieuwsInBeeldView.h"
+
+@implementation NOSNieuwsInBeeldView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
